5e872b62c9f16640c014a03ec840ae8f05c79e8e
hbdoy <felicity860128@gmail.com>
2018-10-31 23:48:12 +0800

    登入後提醒綁定

--- js/function.js
+++ js/function.js
@@ -1,28 +1,27 @@
-/* Login In Form Modal */
 $(document).ready(function () {
+  /* Login In Form Modal */
   $('.modal').modal();
-})
 
-var base64image;
-/* Upload Image */
-function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-      base64image = e.target.result;
-      $('#imagePreview').hide();
-      $('#imagePreview').fadeIn(650);
+  var base64image;
+  /* Upload Image */
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+        base64image = e.target.result;
+        $('#imagePreview').hide();
+        $('#imagePreview').fadeIn(650);
+      }
+      reader.readAsDataURL(input.files[0]);
     }
-    reader.readAsDataURL(input.files[0]);
   }
-}
-$("#register_imageUpload").change(function () {
-  readURL(this);
-});
 
-/* POST JSON */
-$(document).ready(function () {
+  $("#register_imageUpload").change(function () {
+    readURL(this);
+  });
+
+  // 註冊
   $("#submitregister").click(function (e) {
     e.preventDefault();
     $.ajax({
@@ -47,4 +46,35 @@ $(document).ready(function () {
       }
     });
   });
+
+  // 登入
+  $("#signin_btn").click(function (e) {
+    e.preventDefault();
+    if ($("#signin_account").val() != "" && $("#signin_password").val() != "") {
+      $.ajax({
+        type: 'POST',
+        url: `https://xn--pss23c41retm.tw/api/user/${$("#signin_account").val()}`,
+        data: {
+          password: $("#signin_password").val()
+        },
+        success: function (result) {
+          console.log(result.status);
+          if (result.status) {
+            setCookie("key", $("#signin_account").val(), 6);
+            setCookie("value", sha256($("#signin_account").val() + s), 6);
+            window.location.href = './index.html';
+            console.log("驗證完成");
+          }
+        },
+        error: function (error) {
+          console.log(error);
+          alert("發生錯誤");
+        }
+      });
+    } else {
+      alert("帳號密碼不得為空");
+    }
+  });
+
+
 });
--- js/loginpage.js
+++ js/loginpage.js
@@ -1,12 +1,11 @@
-/* Modal */
 $(document).ready(function () {
   $('.modal').modal();
-})
+  $('.collapsible').collapsible();
+  $('.fixed-action-btn').floatingActionButton();
 
-/* Modify -> Get JSON */
-$(document).ready(function () {
+  /* Modify -> Get JSON */
   $.ajax({
-    url: "https://暨大猴子.tw/api/user/104213083",
+    url: `https://暨大猴子.tw/api/user/${getCookie("key")}`,
     type: "GET",
     dataType: "json",
     success: function (data) {
@@ -22,64 +21,58 @@ $(document).ready(function () {
         $('#modify_cardid').val(allcard);
       }
       $('#modify_cellphone').val(data.cellphone);
+      if(data.lineUserID){
+        $("#notify").html(`
+          <div class="input-field col l10 offset-l1 m10 offset-m1 s10 offset-s1">
+            <input type="text" class="validate" value="${data.lineUserID}" readonly>
+            <label class="active" for="modify_email">Please bind the LineBot instantly!</label>
+          </div>
+        `);
+      }
     },
     error: function () {
       alert("ERROR!!!");
     }
-  });
-});
+  })
 
-/* warning */
-$(document).ready(function () {
-  $('.collapsible').collapsible();
-});
+  /* Upload Image */
+  var base64photo;
 
-/* Upload Image */
-var base64photo;
-function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      base64photo = e.target.result;
-      $('#modify_imagePreview').css('background-image', 'url(' + e.target.result + ')');
-      $('#modify_imagePrevieww').hide();
-      $('#modify_imagePreview').fadeIn(650);
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        base64photo = e.target.result;
+        $('#modify_imagePreview').css('background-image', 'url(' + e.target.result + ')');
+        $('#modify_imagePrevieww').hide();
+        $('#modify_imagePreview').fadeIn(650);
+      }
+      reader.readAsDataURL(input.files[0]);
     }
-    reader.readAsDataURL(input.files[0]);
   }
-}
-$("#modify_imageUpload").change(function () {
-  readURL(this);
-});
 
-/* FAB */
-$(document).ready(function () {
-  $('.fixed-action-btn').floatingActionButton();
-});
+  $("#modify_imageUpload").change(function () {
+    readURL(this);
+  })
 
-document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-  var instances = M.FloatingActionButton.init(elems, {
-    direction: 'top',
-    hoverEnabled: false
-  });
-});
-
-/* PATCH JSON */
-$(document).ready(function () {
+  // 修改
   $("#submitmodify").click(function (e) {
     e.preventDefault();
+    if($("#modify_password").val() != "" && $("#modify_password").val() != $("#modify_cpassword").val()){
+      alert("新密碼兩次輸入不一致");
+      return;
+    }
     $.ajax({
       asyn: true,
       crossDomain: true,
       type: 'PATCH',
-      url: 'https://暨大猴子.tw/api/register/104213083',
+      url: `https://暨大猴子.tw/api/register/${getCookie("key")}`,
       data: JSON.stringify({
         "photo": base64photo,
         "name": $("#modify_name").val(),
         "email": $("#modify_email").val(),
         "password": $("#before_password").val(),
-        "newpassword": $("#modify_password").val(),
+        "newpassword": $("#modify_password").val() != ""? $("#modify_password").val():"",
         "cellphone": $("#modify_cellphone").val()
       }),
       headers: {
@@ -88,11 +81,26 @@ $(document).ready(function () {
       },
       success: function (data) {
         console.log(data);
+        alert("修改成功");
+        location.reload();
       },
       error: function (data) {
         // console.log(data.password);
         alert(data.responseJSON.message);
       }
     });
+  });
+
+  $("#logoutBtn").click(function(e){
+    e.preventDefault();
+    logout();
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'top',
+    hoverEnabled: false
   });
 });
