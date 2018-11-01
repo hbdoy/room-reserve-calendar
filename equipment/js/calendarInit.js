cc3acdae4410941c8f3c17368b0b53d3245765c0
hbdoy <felicity860128@gmail.com>
2018-11-01 12:13:48 +0800

    新增物品畫面

---
+++ equipment/js/calendarInit.js
@@ -0,0 +1,155 @@
+$(document).ready(function () {
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        nowIndicator: true,
+        // height: 650,
+        // contentHeight: 600,
+        handleWindowResize: false, // 拖曳瀏覽器視窗時，是否動態調整大小
+        navLinks: true, // can click day/week names to navigate views
+        selectable: true,
+        selectHelper: true,
+        select: function (start, end) {
+            $("#form-btn").click();
+            // var title = prompt('Event Title:');
+            // var eventData;
+            // if (title) {
+            //     eventData = {
+            //         title: title,
+            //         start: start,
+            //         end: end
+            //     };
+            //     // 將新排成加到日曆上
+            //     $('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+            // }
+            // $('#calendar').fullCalendar('unselect');
+
+            // 取出所有排成
+            // var all = $("#calendar").fullCalendar('clientEvents')
+
+            // 取出所有事件源(動態增加的不算)
+            // var test = $('#calendar').fullCalendar('getEventSources');
+
+            // 刪除所有事件源
+            // $('#calendar').fullCalendar('removeEventSources');
+
+            // 添加事件到事件源，因為 renderEvent 不會列入事件源
+            // $('#calendar').fullCalendar('addEventSource', [{start: "2018-09-10", title: "資管系"}]);
+        },
+        eventClick: function (event, element) {
+
+            // console.log(event, element);
+
+            // event.title = "CLICKED!";
+
+            // 重新 render 某個排成
+            // $('#calendar').fullCalendar('updateEvent', event);
+
+            // 重新 render 多個事件
+            // .fullCalendar('updateEvents', events)
+
+            // 刪除排成
+            // $('#calendar').fullCalendar('removeEvents', event._id);
+
+        },
+        // 排成拖動到新時間觸發
+        eventDrop: function (event, delta, revertFunc) {
+
+            alert(event.title + " was dropped on " + event.start.format());
+
+            if (!confirm("Are you sure about this change?")) {
+                revertFunc();
+            }
+
+        },
+        // 調整排成時間觸發
+        eventResize: function (event, delta, revertFunc) {
+
+            alert(event.title + " end is now " + event.end.format());
+
+            if (!confirm("is this okay?")) {
+                revertFunc();
+            }
+
+        },
+        editable: true, // allow the time of event can be changed
+        eventLimit: true, // allow "more" link when too many events
+        // events: [{
+        //         title: '觀餐系',
+        //         start: '2018-09-01'
+        //     },
+        //     {
+        //         title: '資管系',
+        //         start: '2018-09-02',
+        //         end: '2018-09-05'
+        //     },
+        //     {
+        //         title: '經濟系',
+        //         start: '2018-09-09T16:00',
+        //         end: '2018-09-09T18:00'
+        //     },
+        //     {
+        //         title: '國企系',
+        //         start: '2018-09-09T09:00:00',
+        //         end: '2018-09-09T12:00:00'
+        //     },
+        //     {
+        //         title: 'Conference',
+        //         start: '2018-03-11',
+        //         end: '2018-03-13'
+        //     },
+        //     {
+        //         title: 'Meeting',
+        //         start: '2018-03-12T10:30:00',
+        //         end: '2018-03-12T12:30:00'
+        //     },
+        //     {
+        //         title: 'Lunch',
+        //         start: '2018-03-12T12:00:00'
+        //     },
+        //     {
+        //         title: 'Meeting',
+        //         start: '2018-03-12T14:30:00'
+        //     },
+        //     {
+        //         title: 'Happy Hour',
+        //         start: '2018-03-12T17:30:00'
+        //     },
+        //     {
+        //         title: 'Dinner',
+        //         start: '2018-03-12T20:00:00'
+        //     },
+        //     {
+        //         title: 'Birthday Party',
+        //         start: '2018-03-13T07:00:00'
+        //     },
+        //     {
+        //         title: 'Click for Google',
+        //         url: 'http://google.com/',
+        //         start: '2018-03-28'
+        //     }
+        // ],
+
+        // 透過函數取得
+        // events: function (start, end, timezone, callback) {
+            // $.ajax({
+            //     ...
+            //     success: function (doc) {
+            //         var events = [];
+            //         $(doc).find('event').each(function () {
+            //             events.push({
+            //                 title: $(this).attr('title'),
+            //                 start: $(this).attr('start') // will be parsed
+            //             });
+            //         });
+            //         callback(events);
+            //     }
+            // });
+            // callback([{start: "2018-09-09", title: "財金系"}]);
+        // }
+    });
+
+});
---
+++ equipment/js/script.js
@@ -0,0 +1,249 @@
+$(document).ready(function () {
+    M.AutoInit();
+
+    var roomTitle = "",
+        roomId = 0,
+        itemID = "";
+
+    // 選擇院別
+    window.onhashchange = function () {
+        // console.log(location.hash);
+        var hash = location.hash.replace("#", "");
+        switch (hash) {
+            case 'm':
+            case 'h':
+            case 't':
+            case 'e':
+                renderRoomSelect(hash);
+                break;
+        }
+        location.hash = "";
+    }
+
+    function renderRoomSelect(type) {
+        let college = {
+            m: "管理學院",
+            h: "人文學院",
+            t: "科技學院",
+            e: "教育學院"
+        };
+        $('#calendar').fullCalendar('removeEventSources');
+        roomTitle = college[type];
+        $("#header-text").html(`${roomTitle[0]}院`);
+        classRoomTemplate(roomTitle[0]);
+    }
+
+    // 選取教室時觸發
+    $('#room-selection').change(function () {
+        if (!isNaN(this.value) && this.value != "") {
+            $('#calendar').fullCalendar('removeEventSources');
+            console.log(this.value);
+            roomId = this.value;
+            // getEvents();
+            $("#header-text").html(`${roomTitle[0]}院-${roomId}`);
+            itemTemplate();
+        } else {
+            $("#header-text").html(`物品借閱`);
+        }
+    });
+
+    // 選取物品時觸發
+    $('#item-selection').change(function () {
+        if (this.value != "") {
+            $('#calendar').fullCalendar('removeEventSources');
+            console.log(this.value);
+            itemID = this.value;
+            getEvents();
+            $("#header-text").html(`${itemID}`);
+        } else {
+            $("#header-text").html(`物品借閱`);
+        }
+    });
+
+    function getEvents() {
+        $.ajax({
+            url: `https://xn--pss23c41retm.tw/api/item/reservation/${roomTitle}/${roomId}/${itemID}`,
+            type: 'GET',
+            error: function (xhr) {
+                alert('Ajax request 發生錯誤');
+            },
+            success: function (res) {
+                console.log(res);
+                var eventData = [];
+                for (const key in res) {
+                    if (res.hasOwnProperty(key)) {
+                        for (const inner_key in res[key]) {
+                            if (res[key].hasOwnProperty(inner_key)) {
+                                eventData.push(res[key][inner_key]);
+                            }
+                        }
+                    }
+                }
+                console.log(eventData);
+                $('#calendar').fullCalendar('addEventSource', eventData);
+            }
+        });
+    }
+
+    function classRoomTemplate(tag) {
+        $.ajax({
+            url: `https://xn--pss23c41retm.tw/api/item/reservation/${roomTitle}`,
+            type: 'GET',
+            error: function (xhr) {
+                alert('Ajax request 發生錯誤');
+            },
+            success: function (res) {
+                // console.log(res);
+                let str = `<option value="" disabled selected>Choose your option</option>`;
+                for (const key in res) {
+                    if (res.hasOwnProperty(key)) {
+                        str += `<option value="${key}">${tag} ${key}</option>`;
+                    }
+                }
+                $("#room-selection").html(str);
+            }
+        });
+    }
+
+    function itemTemplate() {
+        $.ajax({
+            url: `https://xn--pss23c41retm.tw/api/item/${roomTitle}/${roomId}`,
+            type: 'GET',
+            error: function (xhr) {
+                alert('Ajax request 發生錯誤');
+            },
+            success: function (res) {
+                let str = `<option value="" disabled selected>Choose your option</option>`;
+                for (const key in res) {
+                    if (res.hasOwnProperty(key)) {
+                        str += `<option value="${key}">${res[key].itemName}</option>`;
+                    }
+                }
+                $("#item-selection").html(str);
+            }
+        });
+    }
+
+    $("#form-btn").click(renderForm);
+
+    function renderForm(e) {
+        e.preventDefault();
+        let alertText = "";
+        if (roomId == 0 || roomTitle == "" || itemID == "") {
+            alertText = `<h4 class="center-align">請選擇院別/教室!</h4>`;
+            $(".modal-footer").html(`
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancel</a>`);
+        } else {
+            alertText = `
+            <div style="padding-bottom:10px">
+                <span style="font-size:26px;">借閱資訊</span>
+            </div>
+            <div class="chip">
+                ${roomTitle[0]}${roomId} - ${itemID}
+            </div>
+            <input type="text" id="start-date" class="datepicker" placeholder="Start-Date">
+            <input type="text" id="start-time" class="timepicker" placeholder="Start-Time">
+            <input type="text" id="end-time" class="timepicker" placeholder="End-Time">
+            <div class="input-field">
+                <input type="text" id="studentID" name="studentID">
+                <label for="studentID">Student ID</label>
+            </div>
+            <div class="input-field">
+                <input type="text" id="title" name="title">
+                <label for="title">Describe</label>
+            </div>
+            `;
+            $(".modal-footer").html(`
+            <div class="modalbtn">
+            <a href="#!" class="modal-close waves-effect btn-flat">Cancel</a>
+            <button id="subBtn" class="btn waves-effect">
+            Send</button>`);
+        }
+        $("#reserve-form").html(alertText);
+        M.Datepicker.init(document.querySelectorAll('.datepicker'), {
+            container: '.wrapper',
+            autoClose: true,
+            format: 'yyyy-mm-dd',
+            showClearBtn: true
+        });
+        M.Timepicker.init(document.querySelectorAll('.timepicker'), {
+            container: '.wrapper',
+            autoClose: true,
+            twelveHour: false
+        });
+        M.FormSelect.init(document.querySelectorAll('select'));
+        // $('.timepicker').timepicker();
+        // 手動開啟 modal
+        var instance = M.Modal.getInstance(document.getElementById("modal1"));
+        instance.open();
+    }
+
+    $(document).on('click', '#subBtn', submitForm);
+
+    function submitForm() {
+        var sdate = $("#start-date").val();
+        var stime = $("#start-time").val();
+        var etime = $("#end-time").val();
+        var userID = $("#studentID").val();
+        var des = $("#title").val();
+        var eventData = {};
+        eventData.itemID = itemID;
+        console.log(itemID, sdate, stime, etime, userID, des);
+        if (!(/(\d{4})-(\d{2})-(\d{2})/.test(sdate)) || sdate == "") {
+            alert("起始日期格式錯誤");
+            return;
+        } else {
+            eventData.start = sdate;
+            eventData.end = sdate;
+        }
+        if (!(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.test(stime))) {
+            alert("開始時間格式錯誤");
+            return;
+        }
+        if (!(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.test(etime))) {
+            alert("結束時間格式錯誤");
+            return;
+        }
+        if (stime > etime) {
+            alert("結束時間小於開始時間");
+            return;
+        } else {
+            eventData.start += `T${stime}:00`;
+            eventData.end += `T${etime}:00`;
+        }
+        if (des == "") {
+            alert("請輸入描述");
+            return;
+        } else {
+            eventData.title = des;
+        }
+        if (userID == "") {
+            alert("請輸入學號");
+            return;
+        } else {
+            eventData.studentID = userID;
+        }
+        if (confirm("確定要借閱嗎?")) {
+            console.log(eventData);
+            eventData.repeat = "none";
+            eventData.repeat_end = "none";
+            eventData.type = "inside";
+            eventData.conflict = false;
+            $.ajax({
+                url: `https://xn--pss23c41retm.tw/api/item/reservation/${roomTitle}/${roomId}`,
+                type: 'POST',
+                data: eventData,
+                error: function (xhr) {
+                    alert('Ajax request 發生錯誤');
+                    console.log(xhr);
+                },
+                success: function (res) {
+                    console.log(res);
+                    $('#calendar').fullCalendar('addEventSource', [eventData]);
+                }
+            });
+            var instance = M.Modal.getInstance(document.getElementById("modal1"));
+            instance.close();
+        }
+    }
+});
